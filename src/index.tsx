80bbdc634f19d298402e7b31ef3c26fcc03e03dc
mydatahack <thon6thon6@hotmail.com>
2022-10-19 10:21:35 +1100

    Update with HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 
 import { App } from './App'
 import { GlobalStyles } from './GlobalStyles'
@@ -11,12 +11,12 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <GlobalStyles />
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/app-1" element={<AppExample1 />} />
         <Route path="/app-2" element={<AppExample2 />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
